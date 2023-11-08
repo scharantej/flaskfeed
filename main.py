359f2895477deb8831295ce5a0b5203390881c76
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-08 23:04:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,30 @@
  
 from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    articles = [
-        {
-            'title': 'Article 1',
-            'content': 'This is the content of article 1.'
-        },
-        {
-            'title': 'Article 2',
-            'content': 'This is the content of article 2.'
-        },
-        {
-            'title': 'Article 3',
-            'content': 'This is the content of article 3.'
-        }
-    ]
-    return render_template('index.html', articles=articles)
+def home():
+  return render_template('home.html')
 
-@app.route('/article/<int:article_id>')
-def article(article_id):
-    articles = [
-        {
-            'title': 'Article 1',
-            'content': 'This is the content of article 1.'
-        },
-        {
-            'title': 'Article 2',
-            'content': 'This is the content of article 2.'
-        },
-        {
-            'title': 'Article 3',
-            'content': 'This is the content of article 3.'
-        }
-    ]
-    article = articles[article_id]
-    return render_template('article.html', article=article)
+@app.route('/news', methods=['POST'])
+def news():
+  # Get the search query from the user.
+  query = request.form.get('query')
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    if request.method == 'GET':
-        return render_template('search.html')
-    else:
-        keyword = request.form['keyword']
-        articles = [
-            {
-                'title': 'Article 1',
-                'content': 'This is the content of article 1.'
-            },
-            {
-                'title': 'Article 2',
-                'content': 'This is the content of article 2.'
-            },
-            {
-                'title': 'Article 3',
-                'content': 'This is the content of article 3.'
-            }
-        ]
-        articles = [article for article in articles if keyword in article['title']]
-        return render_template('index.html', articles=articles)
+  # Make a request to the news API.
+  response = requests.get('https://newsapi.org/v2/top-headlines?q={}&apiKey=YOUR_API_KEY'.format(query))
+
+  # Parse the JSON response.
+  data = response.json()
+
+  # Extract the articles from the JSON response.
+  articles = data['articles']
+
+  # Render the news template with the articles.
+  return render_template('news.html', articles=articles)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run()
