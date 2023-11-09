e6d9b92dc7b3c7e5f0767b883eeaab3e2c40d009
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-09 16:13:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
  
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -40,28 +40,27 @@ def article(article_id):
     article = articles[article_id]
     return render_template('article.html', article=article)
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'GET':
-        return render_template('search.html')
+        return render_template('login.html')
     else:
-        keyword = request.form['keyword']
-        articles = [
-            {
-                'title': 'Article 1',
-                'content': 'This is the content of article 1.'
-            },
-            {
-                'title': 'Article 2',
-                'content': 'This is the content of article 2.'
-            },
-            {
-                'title': 'Article 3',
-                'content': 'This is the content of article 3.'
-            }
-        ]
-        articles = [article for article in articles if keyword in article['title']]
-        return render_template('index.html', articles=articles)
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'secret':
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Invalid credentials.')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        # TODO: Save the user to the database.
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
